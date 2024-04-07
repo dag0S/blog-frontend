bcc968815b62219e8515f1c2f7e52e7281aada35
Gosudarev Danila <danidagosudarev@gmail.com>
2024-04-07 18:22:51 +0300

    added fullpost data && fetch tags

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -6,6 +6,11 @@ export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
   return data;
 });
 
+export const fetchTags = createAsyncThunk("posts/fetchTags", async () => {
+  const { data } = await axios.get("/tags");
+  return data;
+});
+
 const initialState = {
   posts: {
     items: [],
@@ -22,6 +27,7 @@ const postsSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
+    // posts
     builder.addCase(fetchPosts.pending, (state) => {
       state.posts.items = [];
       state.posts.status = "loading";
@@ -33,6 +39,20 @@ const postsSlice = createSlice({
     builder.addCase(fetchPosts.rejected, (state) => {
       state.posts.items = [];
       state.posts.status = "error";
+    });
+
+    // Tags
+    builder.addCase(fetchTags.pending, (state) => {
+      state.tags.items = [];
+      state.tags.status = "loading";
+    });
+    builder.addCase(fetchTags.fulfilled, (state, action) => {
+      state.tags.items = action.payload;
+      state.tags.status = "loaded";
+    });
+    builder.addCase(fetchTags.rejected, (state) => {
+      state.tags.items = [];
+      state.tags.status = "error";
     });
   },
 });
